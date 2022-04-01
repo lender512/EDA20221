ad5ad254a5a35a35fdce08b53390fb0527d33b66
Lender512 <luis.berrospi@utec.edu.pe>
2022-04-01 13:04:26 -0500

    update main

--- Lab1-BplusTree/src/main.cpp
+++ Lab1-BplusTree/src/main.cpp
@@ -19,31 +19,31 @@ int main() {
     }
 
     int64_t tiempoInseccion = 0;
-    // for (int t=0; t<10; ++t){
-    //     BplusTree* tree = new BplusTree();
+    for (int t=0; t<10; ++t){
+        BplusTree* tree = new BplusTree();
 
-    //     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
-    //     for (int i=0; i < 1000000; ++i){
-    //         tree->insertar(datos[i]);
-    //     }
-
-    //     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-
-    //     // Calcular tiempo
-    //     auto tiempo = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
-    //     tiempoInseccion += tiempo;
-    // }
-
-
-    BplusTree* tree = new BplusTree();
-
-    for (int i=0; i < 1000000; ++i){
+        for (int i=0; i < 1000000; ++i){
             tree->insertar(datos[i]);
+        }
+
+        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+        // Calcular tiempo
+        auto tiempo = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
+        tiempoInseccion += tiempo;
     }
-    std::cout << "Tiempo Inseccion: " << tiempoInseccion << std::endl;
-    vector<int> v = tree->bfs();
-    for (int i=0; i < v.size(); i++) {
-        cout << v[i] << endl;
-    }
+
+
+    // BplusTree* tree = new BplusTree();
+
+    // for (int i=0; i < 1000000; ++i){
+    //         tree->insertar(datos[i]);
+    // }
+    // std::cout << "Tiempo Inseccion: " << tiempoInseccion << std::endl;
+    // vector<int> v = tree->bfs();
+    // for (int i=0; i < v.size(); i++) {
+    //     cout << v[i] << endl;
+    // }
 }
